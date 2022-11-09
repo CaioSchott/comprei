7aad0d9cf90dc7d3027645c1883e6ba128d7a5ee
Caio Victor Schott <99359060+CaioSchott@users.noreply.github.com>
2022-11-09 17:59:51 -0300

    1.1.0

--- src/Componentes/Forms/index.js
+++ src/Componentes/Forms/index.js
@@ -1,36 +1,53 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './style-forms.css';
+import emailjs from '@emailjs/browser'
 
 export default function Forms () {
+    
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('comprei-forms', 'template_szclps3', form.current, 'BELxbOq6hh43YSvYw')
+        .then((result) => {
+            alert("Mensagem enviada com sucesso.")
+        }, (error) => {
+            alert(error.message)
+        });
+        e.target.reset();
+    };
+
     return (
+
         <div className='container flex-form'>
-            <form className='form' method='post'>
+            <form className='form' method='post' ref={form} onSubmit={sendEmail}>
                 <fieldset>
                     <legend>Preencha o formulário</legend>
 
             
                     <label className='title-input' for='nome' >Nome</label><br/>
-                    <input className='input' type='text' name='nome' placeholder='Ex.: Luana Ferreira' /><br/>
+                    <input className='input' type='text' name='nome' placeholder='Ex.: Luana Ferreira' required/><br/>
                 
                     <label className='title-input' for='email'>Email</label><br/>
-                    <input className='input' type='email' name='email' placeholder='Digite seu email principal' /><br/>
+                    <input className='input' type='email' name='email' placeholder='Digite seu email principal' required/><br/>
                     
                     <label className='title-input' for='telefone'>Telefone</label><br/>
-                    <input className='input' type='tel' name='telefone' placeholder='Digite seu telefone principal' /><br/>
+                    <input className='input' type='tel' name='telefone' placeholder='Digite seu telefone principal' required/><br/>
 
                     <label className='title-input' for='cep'>CEP</label><br/>
-                    <input className='input' type='number' name='cep' placeholder='Digite seu CEP' /><br/>
+                    <input className='input' type='number' name='cep' placeholder='Digite seu CEP' required/><br/>
 
                     <label className='title-input' for='cidade'>Cidade</label><br/>
-                    <input className='input' type='text' name='cidade' placeholder='Digite sua cidade' /><br/>
+                    <input className='input' type='text' name='cidade' placeholder='Digite sua cidade' required/><br/>
                     
                     <label className='title-input' for='estado'>Estado</label><br/>
-                    <input className='input' type='text' name='estado' placeholder='Digite seu estado' /><br/>
+                    <input className='input' type='text' name='estado' placeholder='Digite seu estado' required/><br/>
                     
                     <label className='title-input' for='mensagem'>Mensagem</label><br/>
-                    <textarea className='input' name='mensagem'>Como nos conheceu?</textarea><br/>
-                
-                    <button className='btn' type='submit'>Enviar</button>
+                    <textarea className='input' name='mensagem' placeholder='Como nos conheceu?'></textarea><br/>
+
+                   <input className='btn' type="submit" value="Enviar"/>
                 </fieldset>
             </form>
 
